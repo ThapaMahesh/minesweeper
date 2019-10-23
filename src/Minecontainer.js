73b9f11e9252ad6db15a86cb364dad9a6a16ca2a
Mahesh Bahadur Thapa <mahesh.thapa@makingwaves.com>
2019-10-23 16:23:17 +0200

    beautify board, added timer and mine count (need fix)

--- src/Minecontainer.js
+++ src/Minecontainer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {FaUndoAlt, FaGhost, FaFlag} from 'react-icons/fa';
 import Services from './Services';
 import './Minecontainer.css';
 
@@ -10,8 +10,12 @@ class Minecontainer extends Component {
         this.services = new Services();
         this.state = {
             playboard: [],
-            status: 'playing'
+            status: 'playing',
+            mines: 0,
+            timer: 0
         }
+        this.timer = null;
+        this.minesCount = 0;
         this.newBoardData = {};
         this.tempboard = [];
     }
@@ -19,35 +23,46 @@ class Minecontainer extends Component {
     componentDidMount = () => {
         let sides = Number(this.props.sides);
         let mines = Number(this.props.mines);
+        this.minesCount = mines;
         this.newBoardData = this.services.createNewMine(sides, mines);
         this.tempboard = JSON.parse(JSON.stringify( this.newBoardData.tempboard ));
-        this.setState({playboard: this.newBoardData.tempboard});
+        this.setState({playboard: this.newBoardData.tempboard, status: "playing", mines: mines, timer: 0});
     }
 
     componentDidUpdate = (prevprops) => {
         if(this.props.sides !== prevprops.sides){
             let sides = Number(this.props.sides);
             let mines = Number(this.props.mines);
+            this.minesCount = mines;
             this.newBoardData = this.services.createNewMine(sides, mines);
             this.tempboard = JSON.parse(JSON.stringify( this.newBoardData.tempboard ));
-            this.setState({playboard: this.newBoardData.tempboard});
+            this.setState({playboard: this.newBoardData.tempboard, status: "playing", mines: mines, timer: 0});
+        }
+    }
+
+    setTimer = () => {
+        if(this.timer === null){
+            this.timer = setInterval(() => {
+                this.setState({timer: this.state.timer + 1});
+            }, 1000)
         }
     }
 
     checkMine = (row, col) => {
-        console.log(this.newBoardData.actualboard);
-        console.log(this.state.playboard);
-        if(this.newBoardData.actualboard[row][col] !== "*"){
-            // check surrounding and if 8 of them don't have mine open up and check recursively for each of them
-            this.showNeighbor(row, col)
-            this.setState({playboard: JSON.parse(JSON.stringify( this.tempboard )) });
-        }else{
-            // show all mines
-            this.newBoardData.minesPosition.forEach(element => {
-                this.tempboard[element[0]][element[1]] = "*";
-            });
-
-            this.setState({playboard: JSON.parse(JSON.stringify( this.tempboard )), status: 'lose' });
+        this.setTimer();
+        if(this.tempboard[row][col] !== "F"){
+            if(this.newBoardData.actualboard[row][col] !== "*"){
+                // check surrounding and if 8 of them don't have mine open up and check recursively for each of them
+                this.showNeighbor(row, col)
+                this.setState({playboard: JSON.parse(JSON.stringify( this.tempboard )) });
+            }else{
+                // show all mines
+                this.newBoardData.minesPosition.forEach(element => {
+                    this.tempboard[element[0]][element[1]] = "*";
+                });
+                clearInterval(this.timer);
+                this.setState({playboard: JSON.parse(JSON.stringify( this.tempboard )), status: 'lose' });
+            }
         }
     }
 
@@ -55,9 +70,10 @@ class Minecontainer extends Component {
         let list = [];
         let count = 0;
         // check if 8 of the neighbors are empty, if empty show all
+        check_neighbor:
         for (let i = row-1; i < row+2; i++) {
             for (let j = col-1; j < col+2; j++) {
-                if(i < 0 || j < 0 || i > this.props.sides || j > this.props.sides){
+                if(i < 0 || j < 0 || i > (this.props.sides-1) || j > (this.props.sides-1)){
                     continue;
                 }else if(row === i && col === j){
                     continue;
@@ -66,7 +82,7 @@ class Minecontainer extends Component {
                     continue;
                 }else{
                     ++count;
-                    break;
+                    break check_neighbor;
                 }
             }
         }
@@ -74,27 +90,88 @@ class Minecontainer extends Component {
         if(count === 0){
             // none of the neighbor is a mine so show them all
             list.forEach(element => {
-                if(this.tempboard[element[0]][element[1]] === 0){
-                    this.tempboard[element[0]][element[1]] = (this.newBoardData.actualboard[element[0]][element[1]] === 0) ? "" : this.newBoardData.actualboard[element[0]][element[1]];
+                if(this.tempboard[element[0]][element[1]] === 0 || this.tempboard[element[0]][element[1]] === "F"){
                     this.showNeighbor(element[0], element[1]);
                 }
             });
         }
     }
 
-    render(){
+    reset = () => {
+        this.timer = null;
+        clearInterval(this.timer);
+        this.componentDidMount();
+    }
+
+    verifyMines = () => {
+        let count = 0;
+        this.newBoardData.minesPosition.map(eachMine => {
+            if(this.tempboard[eachMine[0]][eachMine[1]] === "F"){
+                ++count;
+            }
+        });
         
+        if(count === this.newBoardData.minesPosition.length){
+            clearInterval(this.timer);
+            this.setState({status: "win"});
+        }
+    }
+
+    mineStatus = (e, row, col) => {
+        e.preventDefault();
+        if(this.tempboard[row][col] === 0){
+            if(this.minesCount !== 0){
+                this.tempboard[row][col] = "F";
+                --this.minesCount;
+            }
+        }
+        else if(this.tempboard[row][col] === "F"){
+            this.tempboard[row][col] = 0;
+            ++this.minesCount;
+        }
+        this.setState({playboard: this.tempboard, mines: this.minesCount});
+        this.verifyMines();
+    }
+
+    render(){
         return (
             <div className="MineContainer">
-                {this.state.playboard.map((rowArray, row) => {
-                return (
-                    <div key={row} className="row">
-                        { rowArray.map((item, col) => <div data-item={item} className={(item === 0) ? "col" : "displaycol"} onClick={() => this.checkMine(row, col)}>{(item === 0) ? "" : item}</div>)}
+                <div className="playContainer">
+                    <div className="settings">
+                        <div className="timer">
+                            {this.state.timer}
+                        </div>
+                        <div>
+                            <button className="reset" onClick={() => this.reset()}><FaUndoAlt /></button>
+                        </div>
+                        <div className="mines">
+                            {this.state.mines}
+                        </div>
                     </div>
-                )
-            })
-            }
-            <Timer />
+
+                    <div className="playboard">
+                        {this.state.playboard.map((rowArray, row) => {
+                        return (
+                            <div key={row} className="row">
+                                { rowArray.map((item, col) => 
+                                    <div data-item={item} className={(item === 0) || (item === "F") ? "col" : "displaycol"} onClick={() => this.checkMine(row, col)} onContextMenu={(e) => this.mineStatus(e, row, col)}>
+                                        {
+                                            (item === 0) ? "" : (
+                                                (item === "*") ? <FaGhost /> : (
+                                                    (item === "F") ? <FaFlag /> : item
+                                                    )
+                                                )}
+                                    </div>
+                                )}
+                            </div>
+                            )
+                        })
+                        }
+                    </div>
+                </div>
+                <div>
+                    you {this.state.status}
+                </div>
             </div>
         );
     }
